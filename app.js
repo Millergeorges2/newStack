a5fafd261b9f1c6394a512d2b88fb34662201574
Miller <Miller@MacBook-Pro.local>
2019-04-10 16:34:01 -0700

    adding handlears

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var express = require('express');
 
+const exphbs = require('express-handlebars');
+
 const app = express();
 
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.send('about');
+});
 const PORT = 5000;
 
-app.listen(PORT,()=> {
+app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
 });
