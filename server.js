46887fb2fce9b7a0c912fffebbaa5e4ff704a95e
Miller <Miller@MacBook-Pro.local>
2019-04-24 13:11:56 -0700

    first commit

--- server.js
+++ server.js
@@ -14,31 +14,7 @@ mongoose.connect('mongodb://localhost/vidjot-dev', {
     console.log(`MongoDB Connected`);
 });
 
-//load idea model
 
-require('./modals/Idea');
-const Idea = mongoose.model('ideas')
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
-
-
-app.get('/', (req, res) => {
-    res.status(200).send('hello')
-});
-
-app.get('/about', (req, res) => {
-    res.status(200).send('hello')
-});
-
-app.get('/ideas/add', (req, res) => {
-    res.status(200).send('connected');
-});
-
-app.post('/ideas', (req,res)=>{
-    res.send('ok');
-})
 
 const PORT = 5000;
 
